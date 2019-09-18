f6d787564c73e984b040e7f6990ace57293c7af7
thuy duong do <thuyduongdo98@gmail.com>
2019-09-18 22:42:33 +0200

    add new file

---
+++ Get Physical Address/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_Physical_Address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process Netprocess = new Process();
+            Netprocess.StartInfo.FileName = "ipconfig.exe";
+            Netprocess.StartInfo.Arguments = "/all";
+            Netprocess.StartInfo.UseShellExecute = false;
+            Netprocess.StartInfo.RedirectStandardOutput = true;
+            Netprocess.StartInfo.CreateNoWindow = true;
+            Netprocess.Start();
+
+            string results = Netprocess.StandardOutput.ReadToEnd();
+            Netprocess.WaitForExit();
+            Console.WriteLine(results);
+            
+           
+
+            string[] lines = results.Split(new string[] { Environment.NewLine },  //tao array string , cat ghep de lay tung dong 1
+                             StringSplitOptions.RemoveEmptyEntries);              // bo qua dong nao k co character gi
+            var GetPhysical = from line in lines
+                              let PAdd = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                              where PAdd.Contains("Physical Address")
+                              //select new 
+
+
+            ////______________________NET STAT_____________________________________
+            // var networkConnection = from line in lines
+            //                         let cols = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            //                         where cols.Count() == 5 
+            //                         select new { A = cols[0], B = cols[1], C = cols[2], PID = cols[4] };
+            //// var getPhysicalAddress = networkConnection.Select(PA => ) 
+
+
+            //foreach (var line in networkConnection)
+            //{
+            //    Console.WriteLine(line);
+            //}
+            //___________________________________________________________________________
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## this is my program 
+**It was supposed to be a get physical address program but i could not complete it**
+### first we create a new process 
+### then get info from the location called ipconfig.exe 
+### get all info using /all 
+
+### RedirectStandardOutput so that it cant be assigned any value
+
+### CreateNoWindow so that the data gathered wont be opened in another window but opened in our console  
+### then we create an array of string to store data, lines by lines , removing all empty lines that doesnt contain any characters  
